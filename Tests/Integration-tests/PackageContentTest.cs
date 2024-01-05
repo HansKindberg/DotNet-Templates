c40b462903ea84af2476e8bebf476278dae24adf
HansKindberg <hans.kindberg@gmail.com>
2024-01-05 15:47:57 +0100

    a

--- Tests/Integration-tests/PackageContentTest.cs
+++ Tests/Integration-tests/PackageContentTest.cs
@@ -11,6 +11,120 @@ namespace IntegrationTests
 		#region Methods
 
 		[Fact]
+		public async Task NuGet_ContentItems_Test()
+		{
+			await Task.CompletedTask;
+
+			using(var packageArchiveReader = new PackageArchiveReader(Global.NuGetPackageFile.FullName))
+			{
+				var contentItems = packageArchiveReader.GetContentItems().ToList();
+				Assert.Equal(5, contentItems.Count);
+
+				const string expectedVersion = "0.0.0.0";
+
+				Assert.False(contentItems[0].HasEmptyFolder);
+				Assert.Equal(343, contentItems[0].Items.Count());
+				Assert.True(contentItems[0].TargetFramework.AllFrameworkVersions);
+				Assert.Equal("Any,Version=v0.0", contentItems[0].TargetFramework.DotNetFrameworkName);
+				Assert.Empty(contentItems[0].TargetFramework.DotNetPlatformName);
+				Assert.Equal("Any", contentItems[0].TargetFramework.Framework);
+				Assert.Equal("any", contentItems[0].TargetFramework.GetShortFolderName());
+				Assert.False(contentItems[0].TargetFramework.HasPlatform);
+				Assert.False(contentItems[0].TargetFramework.HasProfile);
+				Assert.False(contentItems[0].TargetFramework.IsAgnostic);
+				Assert.True(contentItems[0].TargetFramework.IsAny);
+				Assert.False(contentItems[0].TargetFramework.IsPCL);
+				Assert.False(contentItems[0].TargetFramework.IsPackageBased);
+				Assert.False(contentItems[0].TargetFramework.IsSpecificFramework);
+				Assert.False(contentItems[0].TargetFramework.IsUnsupported);
+				Assert.Empty(contentItems[0].TargetFramework.Platform);
+				Assert.Equal(expectedVersion, contentItems[0].TargetFramework.PlatformVersion.ToString());
+				Assert.Empty(contentItems[0].TargetFramework.Profile);
+				Assert.Equal(expectedVersion, contentItems[0].TargetFramework.Version.ToString());
+
+				Assert.False(contentItems[1].HasEmptyFolder);
+				Assert.Equal(50, contentItems[1].Items.Count());
+				Assert.True(contentItems[1].TargetFramework.AllFrameworkVersions);
+				Assert.Equal("ASP.NET,Version=v0.0,Profile=Razor-ExtraLarge", contentItems[1].TargetFramework.DotNetFrameworkName);
+				Assert.Empty(contentItems[1].TargetFramework.DotNetPlatformName);
+				Assert.Equal("ASP.NET", contentItems[1].TargetFramework.Framework);
+				Assert.Equal("aspnet-razor-extralarge", contentItems[1].TargetFramework.GetShortFolderName());
+				Assert.False(contentItems[1].TargetFramework.HasPlatform);
+				Assert.True(contentItems[1].TargetFramework.HasProfile);
+				Assert.False(contentItems[1].TargetFramework.IsAgnostic);
+				Assert.False(contentItems[1].TargetFramework.IsAny);
+				Assert.False(contentItems[1].TargetFramework.IsPCL);
+				Assert.False(contentItems[1].TargetFramework.IsPackageBased);
+				Assert.True(contentItems[1].TargetFramework.IsSpecificFramework);
+				Assert.False(contentItems[1].TargetFramework.IsUnsupported);
+				Assert.Empty(contentItems[1].TargetFramework.Platform);
+				Assert.Equal(expectedVersion, contentItems[1].TargetFramework.PlatformVersion.ToString());
+				Assert.Equal("Razor-ExtraLarge", contentItems[1].TargetFramework.Profile);
+				Assert.Equal(expectedVersion, contentItems[1].TargetFramework.Version.ToString());
+
+				Assert.False(contentItems[2].HasEmptyFolder);
+				Assert.Equal(42, contentItems[2].Items.Count());
+				Assert.True(contentItems[2].TargetFramework.AllFrameworkVersions);
+				Assert.Equal("ASP.NET,Version=v0.0,Profile=Razor-Large", contentItems[2].TargetFramework.DotNetFrameworkName);
+				Assert.Empty(contentItems[2].TargetFramework.DotNetPlatformName);
+				Assert.Equal("ASP.NET", contentItems[2].TargetFramework.Framework);
+				Assert.Equal("aspnet-razor-large", contentItems[2].TargetFramework.GetShortFolderName());
+				Assert.False(contentItems[2].TargetFramework.HasPlatform);
+				Assert.True(contentItems[2].TargetFramework.HasProfile);
+				Assert.False(contentItems[2].TargetFramework.IsAgnostic);
+				Assert.False(contentItems[2].TargetFramework.IsAny);
+				Assert.False(contentItems[2].TargetFramework.IsPCL);
+				Assert.False(contentItems[2].TargetFramework.IsPackageBased);
+				Assert.True(contentItems[2].TargetFramework.IsSpecificFramework);
+				Assert.False(contentItems[2].TargetFramework.IsUnsupported);
+				Assert.Empty(contentItems[2].TargetFramework.Platform);
+				Assert.Equal(expectedVersion, contentItems[2].TargetFramework.PlatformVersion.ToString());
+				Assert.Equal("Razor-Large", contentItems[2].TargetFramework.Profile);
+				Assert.Equal(expectedVersion, contentItems[2].TargetFramework.Version.ToString());
+
+				Assert.False(contentItems[3].HasEmptyFolder);
+				Assert.Equal(38, contentItems[3].Items.Count());
+				Assert.True(contentItems[3].TargetFramework.AllFrameworkVersions);
+				Assert.Equal("ASP.NET,Version=v0.0,Profile=Razor-Medium", contentItems[3].TargetFramework.DotNetFrameworkName);
+				Assert.Empty(contentItems[3].TargetFramework.DotNetPlatformName);
+				Assert.Equal("ASP.NET", contentItems[3].TargetFramework.Framework);
+				Assert.Equal("aspnet-razor-medium", contentItems[3].TargetFramework.GetShortFolderName());
+				Assert.False(contentItems[3].TargetFramework.HasPlatform);
+				Assert.True(contentItems[3].TargetFramework.HasProfile);
+				Assert.False(contentItems[3].TargetFramework.IsAgnostic);
+				Assert.False(contentItems[3].TargetFramework.IsAny);
+				Assert.False(contentItems[3].TargetFramework.IsPCL);
+				Assert.False(contentItems[3].TargetFramework.IsPackageBased);
+				Assert.True(contentItems[3].TargetFramework.IsSpecificFramework);
+				Assert.False(contentItems[3].TargetFramework.IsUnsupported);
+				Assert.Empty(contentItems[3].TargetFramework.Platform);
+				Assert.Equal(expectedVersion, contentItems[3].TargetFramework.PlatformVersion.ToString());
+				Assert.Equal("Razor-Medium", contentItems[3].TargetFramework.Profile);
+				Assert.Equal(expectedVersion, contentItems[3].TargetFramework.Version.ToString());
+
+				Assert.False(contentItems[4].HasEmptyFolder);
+				Assert.Equal(35, contentItems[4].Items.Count());
+				Assert.True(contentItems[4].TargetFramework.AllFrameworkVersions);
+				Assert.Equal("ASP.NET,Version=v0.0,Profile=Razor-Small", contentItems[4].TargetFramework.DotNetFrameworkName);
+				Assert.Empty(contentItems[4].TargetFramework.DotNetPlatformName);
+				Assert.Equal("ASP.NET", contentItems[4].TargetFramework.Framework);
+				Assert.Equal("aspnet-razor-small", contentItems[4].TargetFramework.GetShortFolderName());
+				Assert.False(contentItems[4].TargetFramework.HasPlatform);
+				Assert.True(contentItems[4].TargetFramework.HasProfile);
+				Assert.False(contentItems[4].TargetFramework.IsAgnostic);
+				Assert.False(contentItems[4].TargetFramework.IsAny);
+				Assert.False(contentItems[4].TargetFramework.IsPCL);
+				Assert.False(contentItems[4].TargetFramework.IsPackageBased);
+				Assert.True(contentItems[4].TargetFramework.IsSpecificFramework);
+				Assert.False(contentItems[4].TargetFramework.IsUnsupported);
+				Assert.Empty(contentItems[4].TargetFramework.Platform);
+				Assert.Equal(expectedVersion, contentItems[4].TargetFramework.PlatformVersion.ToString());
+				Assert.Equal("Razor-Small", contentItems[4].TargetFramework.Profile);
+				Assert.Equal(expectedVersion, contentItems[4].TargetFramework.Version.ToString());
+			}
+		}
+
+		[Fact]
 		public async Task NuGet_DependencyGroups_Test()
 		{
 			await Task.CompletedTask;
