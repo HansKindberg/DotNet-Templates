0fea2ec83a647006def4f4848c063f12584a8471
HansKindberg <hans.kindberg@gmail.com>
2024-01-05 07:43:35 +0100

    a

---
+++ Tests/Integration-tests/PackageContentTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace IntegrationTests
+{
+	public class PackageContentTest
+	{
+		#region Methods
+
+		[Fact]
+		public async Task A_First_Test_That_Later_Should_Be_Removed()
+		{
+			await Task.CompletedTask;
+
+			Assert.Equal(1, 1);
+		}
+
+		#endregion
+	}
+}
