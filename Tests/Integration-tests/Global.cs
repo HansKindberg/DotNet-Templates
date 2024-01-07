21f6f3dca90156c49d3cead00e6bf81f2bf12e5f
HansKindberg <hans.kindberg@gmail.com>
2024-01-07 11:59:19 +0100

    a

--- Tests/Integration-tests/Global.cs
+++ Tests/Integration-tests/Global.cs
@@ -5,17 +5,23 @@ namespace IntegrationTests
 {
 	public static class Global
 	{
-		#region Fields
+		#region Primary fields
 
 		public static readonly DirectoryInfo BaseDirectory = new(AppDomain.CurrentDomain.BaseDirectory);
 		public static readonly DirectoryInfo ProjectDirectory = BaseDirectory.Parent!.Parent!.Parent!;
 		public static readonly DirectoryInfo SolutionDirectory = BaseDirectory.Parent!.Parent!.Parent!.Parent!.Parent!;
 		public static readonly Assembly TemplatesAssembly = typeof(Templates.Global).Assembly;
-		public static readonly DirectoryInfo TemplatesProjectDirectory = new(Path.Combine(SolutionDirectory.FullName, "Source", "Templates"));
+		public static readonly DirectoryInfo TemplatesProjectDirectory = new(Path.Combine(SolutionDirectory!.FullName, "Source", "Templates"));
 		public static readonly DirectoryInfo TemplatesProjectOutputDirectory = new(Path.Combine(TemplatesProjectDirectory.FullName, BaseDirectory.Parent!.Parent!.Name, BaseDirectory.Parent!.Name));
+
+		#endregion
+
+		#region Secondary fields
+
 		public static readonly string NuGetPackageName = TemplatesAssembly.GetName().Name!;
 		public static readonly string NuGetPackageVersion = FileVersionInfo.GetVersionInfo(TemplatesAssembly.Location).ProductVersion!;
 		public static readonly FileInfo NuGetPackageFile = new(Path.Combine(TemplatesProjectOutputDirectory.FullName, $"{NuGetPackageName}.{NuGetPackageVersion}.nupkg"));
+
 		#endregion
 	}
 }
